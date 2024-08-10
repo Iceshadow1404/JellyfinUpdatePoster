7d565f8f99a25dfaaa9f57a81390539489c0bd19
trojaner1404 <joel.willig.1@gmail.com>
2024-08-10 23:24:41 +0200

    Added Webhook Support for HomeAssistant @Druidblack

---
+++ src/webhook.py
@@ -0,0 +1,15 @@
+import requests
+from src.utils import *
+
+
+def webhook(HA_WEBHOOK_URL, HA_WEBHOOK_ID):
+    try:
+        webhook_url = f"{HA_WEBHOOK_URL}/api/webhook/{HA_WEBHOOK_ID}"
+        response = requests.post(webhook_url)
+        response.raise_for_status()
+        log("webhook send sucessfully!")
+        if response.status_code != 200:
+            print(f"Statuscode: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error while sending webhook: {e}")
